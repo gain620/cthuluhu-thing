8b224c7c13c9ea60e9bb5aaac2f02d7df6e4b80b
gainchang <gainchang620@gmail.com>
2021-04-26 03:04:55 +0900

    Add todoitem jsx codes

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { FiCheck, FiTrash } from "react-icons/fi";
+import { useTodoDispatch } from '../TodoContext';
 
 const Remove = styled.div`
   display: flex;
@@ -58,11 +59,15 @@ const Text = styled.div`
 `;
 
 function TodoItem({ id, done, text }) {
+  const dispatch = useTodoDispatch();
+  const onToggle = () => dispatch({ type: 'TOGGLE', id });
+  const onRemove = () => dispatch({ type: 'REMOVE', id });
+
   return (
     <TodoItemBlock>
-      <CheckCircle done={done}>{done && <FiCheck />}</CheckCircle>
+      <CheckCircle onClick={onToggle} done={done}>{done && <FiCheck />}</CheckCircle>
       <Text done={done}>{text}</Text>
-      <Remove>
+      <Remove onClick={onRemove}>
         <FiTrash />
       </Remove>
     </TodoItemBlock>
